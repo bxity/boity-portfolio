6aef83779bcc29036c805fbeeb882ac5d33ba014
Boitumelo <boityy.js@gmail.com>
2024-07-10 21:55:55 +0200

    nav.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./nav/Nav.js";
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Nav/>
+    </>
   );
-}
+};
 
 export default App;
